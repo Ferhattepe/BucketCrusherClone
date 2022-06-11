831d15816d35115e8b998390902b008286719f4d
Ferhat TEPE <ferhattepee@gmail.com>
2022-06-11 23:05:24 +0300

    first commit

---
+++ Assets/_Game/Scripts/MovementController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public Rigidbody targetBody;
+    public Joystick joystick;
+    public float forceSpeed;
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            targetBody.velocity = new Vector3(joystick.Horizontal, joystick.Vertical, 0) * forceSpeed;
+        }
+        else
+        {
+            targetBody.velocity = Vector3.zero;
+        }
+
+    }
+}
