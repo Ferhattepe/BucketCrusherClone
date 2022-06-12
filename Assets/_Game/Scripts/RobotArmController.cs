0d56c7a1ab94091ef9c2e1d1888c35eb1574b79b
Ferhat TEPE <ferhattepee@gmail.com>
2022-06-12 16:19:41 +0300

    feat: realtime stick count system added

--- Assets/_Game/Scripts/RobotArmController.cs
+++ Assets/_Game/Scripts/RobotArmController.cs
@@ -1,31 +1,66 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityAtoms.BaseAtoms;
+using System;
 
 public class RobotArmController : MonoBehaviour
 {
     public HingeJoint stickPrefab;
     public HingeJoint fixJoint;
     public Rigidbody headBody;
-    public int stickCount;
-    public List<HingeJoint> currentSticks;
+    public IntReference stickCount;
+    private List<HingeJoint> _currentSticks;
     private void Awake()
     {
-        if (currentSticks.Count < stickCount)
+        _currentSticks = new List<HingeJoint>();
+        AddSticks(stickCount);
+        SetConfigurations();
+    }
+
+    private void SetConfigurations()
+    {
+        fixJoint.connectedBody = _currentSticks[0].GetComponent<Rigidbody>();
+        _currentSticks[_currentSticks.Count - 1].connectedBody = headBody;
+        headBody.position = fixJoint.transform.position + Vector3.up * _currentSticks.Count;
+    }
+
+    public void OnStickCountChanged()
+    {
+        var amount = stickCount - _currentSticks.Count;
+        if (amount > 0)
         {
-            var needStickAmount = stickCount - currentSticks.Count;
-            for (var i = 0; i < needStickAmount; i++)
+            AddSticks(amount);
+        }
+        else if (amount < 0)
+        {
+            RemoveSticks(amount);
+        }
+
+        SetConfigurations();
+    }
+
+    private void RemoveSticks(int amount)
+    {
+        amount = Math.Abs(amount);
+        for (var i = 0; i < amount; i++)
+        {
+            var stick = _currentSticks[0];
+            _currentSticks.RemoveAt(0);
+            Destroy(stick.gameObject);
+        }
+    }
+
+    private void AddSticks(int amount)
+    {
+        for (var i = 0; i < amount; i++)
+        {
+            var stick = Instantiate(stickPrefab, transform);
+            if (_currentSticks.Count > 0)
             {
-                var stick = Instantiate(stickPrefab, transform);
-                if (currentSticks.Count > 0)
-                {
-                    var lastStick = currentSticks[currentSticks.Count - 1];
-                    lastStick.connectedBody = stick.GetComponent<Rigidbody>();
-                }
-                currentSticks.Add(stick);
+                var lastStick = _currentSticks[_currentSticks.Count - 1];
+                lastStick.connectedBody = stick.GetComponent<Rigidbody>();
             }
-            fixJoint.connectedBody = currentSticks[0].GetComponent<Rigidbody>();
-            currentSticks[currentSticks.Count - 1].connectedBody = headBody;
-            headBody.position = fixJoint.transform.position + Vector3.up * currentSticks.Count;
+            _currentSticks.Add(stick);
         }
     }
 }
